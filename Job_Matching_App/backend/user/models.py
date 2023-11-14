0ee99bae5448a79450c84949b859d9321b69a176
Kiran Devihosur <kirandevihosur@Kirans-MacBook-Pro.local>
2023-11-14 15:19:12 -0700

    Adding Frontend Code

--- Job_Matching_App/backend/user/models.py
+++ Job_Matching_App/backend/user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    firstName = models.URLField(unique=True)
+    lastName = models.CharField(max_length=255, null=True)
+    email = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.title
