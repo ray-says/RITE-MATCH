fd3c163cc469505c52bf688a4906ef98504f5180
Kiran Devihosur <kirandevihosur74@gmail.com>
2023-12-09 13:34:12 -0700

    Adding new changes

---
+++ Job_Matching_App/frontend/src/components/layout/Footer.js
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./Footer.module.css";
+import { Box, Typography, Grid, Link,IconButton, Container } from "@mui/material";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { useTheme } from "../layout/ThemeContext";
+
+
+const Footer = (props) => {
+  const { theme, toggleTheme } = useTheme();
+  const currentYear = new Date().getFullYear();
+
+  const footerClass = `${styles.footer} ${theme === "light" ? styles.light : styles.dark}`;
+
+  return (
+    <Box
+      className={footerClass}
+    >
+      <Container maxWidth="lg">
+        <Grid container direction="column" alignItems="center">
+          <Grid item>
+          <Link href="#" sx={{ mx: 2, color: theme === "light" ? "white" : "#03DAC5"  }}>
+            About Us
+          </Link>
+          <Link href="#" sx={{ mx: 2, color: theme === "light" ? "white" : "#03DAC5"  }}>
+            Contact Us
+          </Link>
+          <Link href="#" sx={{ mx: 2, color: theme === "light" ? "white" : "#03DAC5"  }}>
+            Support
+          </Link>
+          <Link href="#" sx={{ mx: 2, color: theme === "light" ? "white" : "#03DAC5"  }}>
+            Carrers
+          </Link>
+        </Grid>
+        <Grid item xs={12}> 
+          <IconButton sx={{ color: theme === "light" ? "white" : "#03DAC5" }} href="https://facebook.com">
+            <FacebookIcon />
+          </IconButton>
+          <IconButton sx={{ color: theme === "light" ? "white" : "#03DAC5" }} href="https://twitter.com">
+            <TwitterIcon />
+          </IconButton>
+          <IconButton sx={{ color: theme === "light" ? "white" : "#03DAC5" }} href="https://instagram.com">
+            <InstagramIcon />
+          </IconButton>
+          <IconButton sx={{ color: theme === "light" ? "white" : "#03DAC5" }} href="https://linkedin.com">
+            <LinkedInIcon />
+          </IconButton>
+        </Grid>
+        <Grid item>
+          <Typography
+            variant="body2"
+            sx={{ display: { xs: "none", sm: "block"}, color: theme === "light" ? "white" : "#03DAC5" }}
+          >
+            Copyright © {currentYear} JobMatch. All rights reserved.
+          </Typography>
+        </Grid>
+        </Grid>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
+
---
+++ Job_Matching_App/frontend/src/components/layout/Header.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styles from "./Header.module.css";
+import { useTheme } from "../layout/ThemeContext";
+import MoonIcon from "../icons/MoonIcon";
+import SunIcon from "../icons/SunIcon";
+import { Button, Tooltip } from "@mui/material";
+
+const Header = (props) => {
+  const { theme, toggleTheme } = useTheme();
+  const headerClass = `${styles.header} ${theme === "light" ? styles.light : styles.dark}`;
+
+  return (
+    <React.Fragment>
+      <header className={headerClass}>
+      <h1 style={{ color: theme === "light" ? "white" : "#03DAC5" }}>Jobs.com</h1>
+      <Tooltip title={`Toggle ${theme === "light" ? "dark" : "light"} mode`} placement="bottom" arrow>
+        <Button
+          onClick={toggleTheme}
+          sx={{
+            borderRadius: '25%', 
+            minWidth: '48px', 
+            width: '48px', 
+            height: '48px',
+            padding: '0',
+            color: theme === "light" ? "black" : "black",
+            backgroundColor: theme === "light" ? "#273746" : "#03DAC5",
+            '&:hover': {
+              backgroundColor: theme === "light" ? "#808B96" : "#808B96",
+            },
+            transform: "translateY(-1px)",
+          }}
+          variant="contained"
+        >
+          {theme === "light" ? <MoonIcon /> : <SunIcon />}
+        </Button>
+        </Tooltip>
+      </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
