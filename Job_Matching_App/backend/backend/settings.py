e79116af3b35b16f7f0081616f02e8b547ebf0ba
Kiran Devihosur <kirandevihosur74@gmail.com>
2023-12-10 19:27:06 -0700

    Updating Changes to settings.py

--- Job_Matching_App/backend/backend/settings.py
+++ Job_Matching_App/backend/backend/settings.py
@@ -52,9 +52,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'backend.urls'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 TEMPLATES = [
     {
